3b23368cb208ebf52aafe0d4cc8f50d51b016376
Omer Ganim <ganimomer@gmail.com>
2016-12-25 17:25:42 +0200

    add library and libraryTarget config to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'index.js'
+    filename: 'index.js',
+    library: 'sendChannelMessage',
+    libraryTarget: 'umd'
   },
   module: {
     loaders: [{
